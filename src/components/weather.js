10c41bf2fff638e3f20a6b74b948ffb958294718
Christian Ndu <christiannduh@gmail.comgit config --global user.email christiannduh@gmail.com>
2020-10-01 06:47:00 +0100

    little styles changes

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,40 +1,62 @@
-import React, { Fragment } from "react";
-import "../App.css"
+import React, { Fragment } from 'react'
+import '../App.css'
 
-export default function Weather ({data,value}){
-return(
-  <Fragment>
-    <div className="location box">
-      <div>
-      <h3>Location Data</h3>
-      <div>Name:  <span>{data.name}</span></div>
-      
-    <br/>
-    <div> Region: <span>{data.region}</span></div>
-      
-    <br/>
-    <div> Country: <span>{data.country}</span></div>
-   
-    <br/>
-    <div> Date & Time: <span>{data.localtime}</span></div>
-    <br/>
-    <div>Wind Degree: <span>{value.wind_degree}</span></div>
-    <br/>
-    <div>Wind Direction: <span>{value.wind_dir}</span></div>
-    <br/>
-    <div>Humidity: <span>{value.humidity}</span></div>
-        <br/>
-      <div>Latitude: <span>{data.lat} </span>
-      Longitude: <span>{data.lon}</span>
-      </div>
-      
-</div>
-<div className="img-div">
-<h2>{value.condition.text}&nbsp;{value.temp_c} &#8451;</h2>
-      <img className="img" src={value.condition.icon} alt="weatherImage" />
-    </div>
-    </div>
-   
-  </Fragment>
-)
+export default function Weather({ data, value }) {
+    return (
+        <Fragment>
+            <div className="location box">
+                <div className="details">
+                    <h3>Location Data</h3>
+                    <div>
+                        Name: <span>{data.name}</span>
+                    </div>
+
+                    <div>
+                        {' '}
+                        Region: <span>{data.region}</span>
+                    </div>
+
+                    <div>
+                        {' '}
+                        Country: <span>{data.country}</span>
+                    </div>
+
+                    <div>
+                        {' '}
+                        Date & Time: <span>{data.localtime}</span>
+                    </div>
+
+                    <div>
+                        Wind Degree: <span>{value.wind_degree}</span>
+                    </div>
+
+                    <div>
+                        Wind Direction: <span>{value.wind_dir}</span>
+                    </div>
+
+                    <div>
+                        Humidity: <span>{value.humidity}</span>
+                    </div>
+
+                    <div>
+                        Latitude: <span>{data.lat} </span>
+                    </div>
+                    <div>
+                        {' '}
+                        Longitude: <span>{data.lon}</span>
+                    </div>
+                </div>
+                <div className="img-div">
+                    <h2>
+                        {value.condition.text}&nbsp;{value.temp_c} &#8451;
+                    </h2>
+                    <img
+                        className="img"
+                        src={value.condition.icon}
+                        alt="weatherImage"
+                    />
+                </div>
+            </div>
+        </Fragment>
+    )
 }
